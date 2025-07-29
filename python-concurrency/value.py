6fba43d086f574c43ab4b4597aa03eaef0b0f1d0
Premanshu Bhagat <premtsapj@gmail.com>
2025-07-29 16:36:56 -0400

    using Value from multiprocessing module to share that value between parent and child process.

---
+++ python-concurrency/value.py
@@ -0,0 +1,20 @@
+# using Value to manipulate a single value shared among the parent and child process.
+
+import multiprocessing
+
+def foo(val):
+    """function to update value from the parent process"""
+
+    print("foo process started")
+    val.value = 3.1415
+    print("foo process ends, value updated.")
+
+if __name__ == "__main__":
+    val = multiprocessing.Value('d',2.718)  # updating e to pi using foo
+    print("val value before updating = {}".format(val.value))
+    p = multiprocessing.Process(target=foo, args=(val,)) 
+
+    p.start()
+    p.join()
+
+    print("updated value updated by foo = {}".format(val.value))
