76134d8f7b1ce37ffec03a5240215a3c2046f554
Premanshu Bhagat <premtsapj@gmail.com>
2025-10-11 13:33:13 -0400

    added constructor

--- cpp-concurrency/high-perf-cpp/section-1/accounts.h
+++ cpp-concurrency/high-perf-cpp/section-1/accounts.h
@@ -5,6 +5,7 @@ class Account
     private:
         double m_balance;
     public:
+        Account( double balance);
         double getBalance();
         void deposit(double amount);
         void withdraw(double amount);
