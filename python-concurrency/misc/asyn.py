d09274a8e0c76c692e0db1660899e1a05ee760f6
Premanshu Bhagat <premtsapj@gmail.com>
2025-11-10 16:59:28 -0500

    added a timer decorator.

--- python-concurrency/misc/asyn.py
+++ python-concurrency/misc/asyn.py
@@ -1,12 +1,23 @@
 #pylint: skip-file
 
+import time
 import asyncio
+
+def timer(func):
+    if asyncio.iscoroutinefunction(func):
+        async def wrapper(*args, **kwargs):
+            start = time.time()
+            await func(*args, **kwargs)
+            elapsed = time.time() - start
+            print(f"Elapsed Time: {elapsed}")
+        return wrapper
 
 async def coroutine(name, delay):
     print(f"running coroutine: {name}")
     await asyncio.sleep(delay)
     print(f"finished  coroutine: {name}")
 
+@timer
 async def main():
     task1 = asyncio.create_task(coro=coroutine("Read Sensors", 2))
     task2 = asyncio.create_task(coro=coroutine("Health Check", 2))
