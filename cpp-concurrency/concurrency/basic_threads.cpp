ea1d4ce1b773793e602bde71677a10cdc4a54e1f
Premanshu Bhagat <premtsapj@gmail.com>
2026-01-12 16:34:28 -0500

    basic threads in C++

---
+++ cpp-concurrency/concurrency/basic_threads.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <thread>
+
+void subsystemCheck(const char* name) {
+    std::cout << "\nChecking subsys: " << name << "\n";
+}
+
+int main(void)
+{
+    std::thread t1(subsystemCheck, "Navigation");
+    std::thread t2(subsystemCheck, "GNC");
+    std::thread t3(subsystemCheck, "Propulsion");
+    std::thread t4(subsystemCheck, "Electronics");
+    std::thread t5(subsystemCheck, "Payload");
+    std::thread t6(subsystemCheck, "Structures");
+
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
+    t5.join();
+    t6.join();
+
+    std::cout << "\nAll subsystems checked\n";
+
+    return EXIT_SUCCESS;
+}
